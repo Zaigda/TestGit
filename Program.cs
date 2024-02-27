6ed1097f24e3e63d5f898755691de19fd011ea05
elyanfear <marc1.teyssier@laposte.net>
2024-02-27 11:38:01 +0100

    Modif Marc
    Nouveau test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification de Marc");
+Console.WriteLine("Modification de Marc à nouveau");
